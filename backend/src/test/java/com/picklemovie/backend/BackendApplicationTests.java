1af8062c2c511888422a60bfbd0bc4f058656223
florenceyay <florenceyay@gmail.com>
2025-01-09 21:42:00 -0500

    creating backend and frontend folder

---
+++ backend/src/test/java/com/picklemovie/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picklemovie.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
